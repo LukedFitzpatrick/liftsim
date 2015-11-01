0387f8a0cba0c70c437a301a1181d45299c405ab
Luke David Fitzpatrick <lukedfitzpatrick@gmail.com>
2015-11-01 16:44:48 +1100

    tried to start physics...

--- gameObject.py
+++ gameObject.py
@@ -1,17 +1,31 @@
 from graphicsHandler import *
 
 class GameObject:
-   def __init__(self, debugName, x, y, graphic=None):
+   def __init__(self, debugName, x, y, graphic=None, physics=None):
       self.debugName = debugName
       self.x = x
       self.y = y
       if(graphic):
          self.graphic = graphic
          self.graphic.parent = self
+      else:
+         self.graphic = None
+      if(physics):
+         self.physics = physics
+         self.physics.parent = self
+      else:
+         self.physics = None
 
    def update(self, gameObjects):
       if(self.graphic):
          self.graphic.update()
+      if(self.physics):
+         self.physics.update(gameObjects)
+
+   def getRect(self):
+      r = pygame.Rect(self.x, self.y, 
+                      self.graphic.width, self.graphic.height)
+      return r
 
       
 class Graphic:
@@ -21,6 +35,10 @@ class Graphic:
       self.frameLifeSpans = frameLifeSpans
       self.priority = priority
       self.currentFrameIndex = 0
+      self.width = self.frames[self.currentFrameIndex].get_width()
+      self.height = self.frames[self.currentFrameIndex].get_height()
+
+      
 
    def update(self):
       # possibly update the animation state here etc
@@ -30,3 +48,17 @@ class Graphic:
                     self.parent.x, self.parent.y, 
                     self.frameLifeSpans[self.currentFrameIndex],
                     self.priority, self.parent.debugName)
+      self.width = self.frames[self.currentFrameIndex].get_width()
+      self.height = self.frames[self.currentFrameIndex].get_height()
+
+
+class Physics:
+   def __init__(self, solid, mobile):
+      self.solid = solid
+      self.mobile = mobile
+   
+   
+      
+   def update(self, gameObjects):
+      x = self.parent.x
+      y = self.parent.y
--- ninjad.py
+++ ninjad.py
@@ -8,12 +8,18 @@ from gameObject import *
 cleanAllLogs()
 pygame.init()
 
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+
 def playGame(screen):
    # temporary level generator here, to be replaced
    gameObjects = []
    playerimage1 = pygame.image.load(os.path.join("graphics/idle1.png"))
-   playergraphic = Graphic([playerimage1], [1], 1)
-   playerobject = GameObject("Player", 100, 100, playergraphic)
+   playergraphic = Graphic([playerimage1], [1], 10)
+   playerphysics = Physics(True, True)
+   playerobject = GameObject("Player", 100, 100, 
+                             playergraphic, playerphysics)
    gameObjects.append(playerobject)
 
    bgi = pygame.image.load(os.path.join("graphics/lightbackground.png"))
@@ -21,14 +27,53 @@ def playGame(screen):
    bgobject = GameObject("Background", 0, 0, bggraphic)
    gameObjects.append(bgobject)
 
-   
+   # blocks on the bottom
+   for x in range(0, SCREEN_WIDTH/32):     
+      xspot = x*32
+      yspot = SCREEN_HEIGHT - 32
+      nameString = "Block at (" + str(xspot) + ", " + str(yspot)+")"
+      blocki = pygame.image.load(os.path.join("graphics/block.png"))
+      blockg = Graphic([blocki], [1], 5)
+      blockp = Physics(True, False)
+      blockobj = GameObject(nameString, xspot, yspot, blockg, blockp)
+      gameObjects.append(blockobj)
+      
+   # blocks on the top
+   for x in range(0, SCREEN_WIDTH/32):     
+      xspot = x*32
+      yspot = 0
+      nameString = "Block at (" + str(xspot) + ", " + str(yspot)+")"
+      blocki = pygame.image.load(os.path.join("graphics/block.png"))
+      blockg = Graphic([blocki], [1], 5)
+      blockp = Physics(True, False)
+      blockobj = GameObject(nameString, xspot, yspot, blockg, blockp)
+      gameObjects.append(blockobj)
 
+   # blocks on the left
+   for y in range(0, SCREEN_HEIGHT/32):     
+      xspot = 0
+      yspot = y*32
+      nameString = "Block at (" + str(xspot) + ", " + str(yspot)+")"
+      blocki = pygame.image.load(os.path.join("graphics/block.png"))
+      blockg = Graphic([blocki], [1], 5)
+      blockp = Physics(True, False)
+      blockobj = GameObject(nameString, xspot, yspot, blockg, blockp)
+      gameObjects.append(blockobj)
+
+   # blocks on the right
+   for y in range(0, SCREEN_HEIGHT/32):     
+      xspot = SCREEN_WIDTH - 32
+      yspot = y*32
+      nameString = "Block at (" + str(xspot) + ", " + str(yspot)+")"
+      blocki = pygame.image.load(os.path.join("graphics/block.png"))
+      blockg = Graphic([blocki], [1], 5)
+      blockp = Physics(True, False)
+      blockobj = GameObject(nameString, xspot, yspot, blockg, blockp)
+      gameObjects.append(blockobj)
 
    playLevel(gameObjects, screen)
 
 
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
